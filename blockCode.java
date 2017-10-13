922a342488e262926c2c74c4d5f8545cb1f6cedd
cscssei <elainefacade@gmail.com>
2017-10-12 22:32:20 -0700

    Update blockCode.java

--- blockCode.java
+++ blockCode.java
@@ -8,20 +8,37 @@ public class blockCode {
         int[] arr2 = {2,5,5,9,1,3};
         int len1 = arr1.length;
         int len2 = arr2.length;
-        int[] sum = new int[Math.max(len1, len2)];
+        int[] sum = new int[Math.max(len1, len2)+1];
 
-        if (len1 > len2) {
-            sum = arr1;
-        } else {
-            sum = arr2;
+        int cover = 0;
+        int i = len1 - 1, j = len2 - 1, k = sum.length - 1;
+        while (i >= 0 && j >=0) {
+            sum[k] = (arr1[i] + arr2[j]) % 10 + cover;
+            cover = (arr1[i] + arr2[j]) / 10;
+            i--;
+            j--;
+            k--;
         }
 
-        for (int i = 0; i < len1 && i < len2; i++) {
-            sum[i] = arr1[i] + arr2[i];
-            sum[i] = sum[i] < 10 ? sum[i] : sum[i] > 10 ? (sum[i] - 10) : 1;
-            int j = i;
+        if (i > 0){
+            while (i >= 0){
+                sum[k] = arr1[i] + cover;
+                cover = 0;
+                i--;
+                k--;
+            }
         }
-
+        else{
+            while (j >= 0) {
+                sum[k] = arr2[j] + cover;
+                cover = 0;
+                j--;
+                k--;
+            }
+        }
+        if (sum[k] == 0){
+            sum = Arrays.copyOfRange(sum, 1, sum.length);
+        }
         System.out.println(Arrays.toString(sum));
     }
 }
