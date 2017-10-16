10f20c5dd903a341de67ccea91c13c8775c27c11
cscssei <elainefacade@gmail.com>
2017-10-15 19:08:25 -0700

    Update RotateArray.java

--- RotateArray.java
+++ RotateArray.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
 public class RotateArray {
+    // rotate the array for a specific range
     public static int[] reverse(int[] nums, int i, int j){
         for(; i<=j; i++, j--) {
             int temp = nums[i];
@@ -12,8 +13,11 @@ public class RotateArray {
 
     public static void rotate(int[] nums, int k) {
         k = k % nums.length;
+        // rotate the first part - [4,3,2,1,5,6,7]
         reverse(nums, 0, nums.length - k - 1);
+        // rotate the second part - [4,3,2,1,7,6,5]
         reverse(nums, nums.length - k, nums.length - 1);
+        // rotate the whole array - [5,4,3,1,2,3,4]
         reverse(nums, 0, nums.length - 1);
 
         return;
